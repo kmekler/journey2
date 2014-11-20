45b2502bc954b54df9fc18b9cf11ba79d2fdbff1
kmekler <kirby.lodin@gmail.com>
2014-11-19 23:46:51 -0800

    app now has authentication and fitgem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Journey2::Application.config.secret_key_base = 'a33c3d2733cb5a97b26109e133f6532c7109f5a5aa53bb67417e03f52bdf884921e0b841324643e61a75a4f1ea11ac891f92d416c60ac04e02214794ec872e25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Journey2::Application.config.session_store :cookie_store, key: '_journey2_session'
