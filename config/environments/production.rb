58cb498f906f0cc362996a26f9400a17b0498226
kmekler <kirby.lodin@gmail.com>
2014-11-22 16:08:06 -0800

    trying to get heroku to work with jquery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Journey2::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  # was false - may need to be changed back
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
